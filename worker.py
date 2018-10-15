5d6c05e28a8d6373abe7e95c8d802877b1e7c044
Sir-Cool <dan@grahamnaylor.net>
2018-10-15 16:10:59 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -18,10 +18,10 @@ async def on_message(message):
     if message.content.startswith('!calculate 2x3'):
         await client.send_message(message.channel, '2x3=1 (modulo 5) obviously')
     elif message.content.startswith('!calculate'):
-        modulus = str(random.randint(0, 10))
-        #exec('calculate = str(' + ' '.join(message.content.split(' ')[1:]).replace('x', '*') + ' % ' + modulus + ') + " (modulo ' + modulus + ' obviously)"')
-        #await client.send_message(message.channel, calculated)
-        await client.send_message(message.channel, 'calculate = str(' + ' '.join(message.content.replace('x', '*').split(' ')[1:]).replace('x', '*') + ' % ' + modulus + ') + " (modulo ' + modulus + ' obviously)"')
+        modulus = str(random.randint(2, 10))
+        exec('calculate = str((' + ' '.join(message.content.replace('x', '*').split(' ')[1:]).replace('x', '*') + ') % ' + modulus + ') + " (modulo ' + modulus + ' obviously)"')
+        await client.send_message(message.channel, calculated)
+        #await client.send_message(message.channel, '')
         
 @client.event
 async def on_ready():
