f57a85d41def55f61a168d80dddbb6a08d57d100
Sir-Cool <dan@grahamnaylor.net>
2018-10-15 18:20:45 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -3,6 +3,22 @@ import discord
 import random
 import math
 
+def remove_zeroes(exp):
+    expression = exp.lstrip('0')
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    changed = True
+    while changed:
+        changed = False
+        for i in range(len(expression)):
+            if not(changed):
+                if expression[i] == '0':
+                    if not(expression [i - 1] in numbers) and (expression[i + 1] in numbers):
+                        expression = expression[0:i] + expression[i+1:]
+                    else:
+                        expression = expression[0:i] + '!"zero"!' + expression[i+1:]
+                    changed = True
+    return(expression.replace('!"zero"!', '0'))
+
 global calculated
 calculated = 'not calculated'
 
@@ -25,9 +41,13 @@ async def on_message(message):
     #if message.content.startswith('!calculate 2x3'):
     #    await client.send_message(message.channel, '2x3=1 (modulo 5) obviously')
     if message.content.startswith('!calculate'):
-        modulus = str(random.randint(2, 10))
+        params = message.content.replace('modulo', 'mod').split(' mod ')
+        if len(params) == 2:
+            modulus = params[1].lstrip('0')
+        else:
+            modulus = str(random.randint(2, 10))
         #exec('calculated = ' +  ' '.join(message.content.replace('x', '*')[11:]).replace('x', '*'))
-        exec('global calculated\ncalculated = str((' + reverse(reverse(message.content.lower().replace('x', '*').replace('sqrt', 'math.sqrt')[11:])) + ') % ' + modulus + ') + " (modulo ' + modulus + ')"')
+        exec('global calculated\ncalculated = str((' + remove_zeroes(params[0].lower().replace('x', '*').replace('sqrt', 'math.sqrt')[11:]) + ') % ' + modulus + ') + " (modulo ' + modulus + ')"')
         #await client.send_message(message.channel, 'calculated = ' +  ' '.join(message.content.replace('x', '*')[11:]).replace('x', '*'))
         await client.send_message(message.channel, message.content[11:] + ' = ' + str(calculated))
         
