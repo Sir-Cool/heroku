5686df0dba60b77d79e2c013a7a5a0532b4d3203
Sir-Cool <dan@grahamnaylor.net>
2018-10-17 16:29:41 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -4,6 +4,7 @@ import random
 import math
 import traceback
 import re
+import time
 
 def remove_zeroes(exp):
     """expression = exp.lstrip('0')
@@ -34,46 +35,69 @@ def reverse(string):
 
 @client.event
 async def on_message(message):
-    await client.change_presence(status=discord.Status.dnd, game=discord.Game(name='Working on: '+message.content))
-    #await client.change_presence(game=discord.Game(name='type !help', type='Testing'))
-    # we do not want the bot to reply to itself
+    msg = message.content
     if message.author == client.user:
         return
-
-    if message.content.startswith('!shuffle'):
-        msg = 'Shuffling'
+    await client.change_presence(status=discord.Status.dnd, game=discord.Game(name='Working on: '+msg))
+    #await client.change_presence(game=discord.Game(name='type !help', type='Testing'))
+    # we do not want the bot to reply to itself
+    
+    if msg.startswith('!shuffle'):
+        #msg = 'Shuffling'
         await client.send_file(message.channel, './ezgif.com-video-to-gif.gif')
-    #if message.content.startswith('!calculate 2x3'):
+        last_command = 'Shuffle'
+    msg = message.content
+    #if msg.startswith('!calculate 2x3'):
     #    await client.send_message(message.channel, '2x3=1 (modulo 5) obviously')
-    if message.content.startswith('!help'):
-        if message.content == '!help':
+    msg = message.content
+    if 'mao' in msg.lower():
+        await client.send_message(message.channel, 'Taking Mao\'s name in vain, +5 cards.')
+        last_command = 'Penalising taking Mao\'s name in vain.'
+    msg = message.content
+    if '?' in msg.lower():
+        await client.send_message(message.channel, 'Asking questions, +1 card.')
+        last_command = 'Penalising asking questions.'
+    msg = message.content
+    if 'martin' in msg.lower() and not('respect' in msg.lower()):
+        await client.send_message(message.channel, 'Failure to pay respect to Martin, +1 card.')
+        last_command = 'Penalising failure to respect Martin.'
+    msg = message.content
+    if msg.startswith('!help'):
+        if msg == '!help':
             await client.send_message(message.channel, """I am Martin, I like to shuffle (!shuffle)
 I also like modular arithmetic! (!calculate [ mod <modulus>]) <- square brackets indicate optional parameter
     Enter expressions using the following symbols: + - x / ^ and the following functions: sqrt() fact()""")
-    if message.content.startswith('!calculate'):
+            last_command = '!help'
+    msg = message.content
+    if msg.startswith('!calculate'):
         global calculated
         modulus = None
-        params = message.content.replace('modulo', 'mod').split(' mod ')
+        params = msg.replace('modulo', 'mod').split(' mod ')
         if len(params) == 2:
             modulus = params[1].lstrip('0')
+        msg = message.content
         #else:
         #    modulus = str(random.randint(2, 10))
-        #exec('calculated = ' +  ' '.join(message.content.replace('x', '*')[11:]).replace('x', '*'))
+        #exec('calculated = ' +  ' '.join(msg.replace('x', '*')[11:]).replace('x', '*'))
         try:
             if modulus == None:
-                exec('global calculated\ncalculated = "' + message.content[11:] + ' = " + str((' + remove_zeroes(params[0].lower().replace('x', '*').replace('sqrt', 'math.sqrt').replace('fact', 'math.factorial').replace('^', '**')[11:]) + '))')
+                exec('global calculated\ncalculated = "' + msg[11:] + ' = " + str((' + remove_zeroes(params[0].lower().replace('x', '*').replace('sqrt', 'math.sqrt').replace('fact', 'math.factorial').replace('^', '**')[11:]) + '))')
             else:
-                exec('global calculated\ncalculated = "' + message.content[11:] + ' = " + str((' + remove_zeroes(message.content.lower().replace('mod', '%').replace('x', '*').replace('sqrt', 'math.sqrt').replace('fact', 'math.factorial').replace('^', '**')[11:]) + ')) + " (modulo ' + modulus + ')"')
+                exec('global calculated\ncalculated = "' + msg[11:] + ' = " + str((' + remove_zeroes(msg.lower().replace('mod', '%').replace('x', '*').replace('sqrt', 'math.sqrt').replace('fact', 'math.factorial').replace('^', '**')[11:]) + ')) + " (modulo ' + modulus + ')"')
         except Exception:
-            calculated = '@Daniel```' + traceback.format_exc() + '```'
-        #await client.send_message(message.channel, 'calculated = ' +  ' '.join(message.content.replace('x', '*')[11:]).replace('x', '*'))
+            calculated = '```' + traceback.format_exc() + '```'
+        #await client.send_message(message.channel, 'calculated = ' +  ' '.join(msg.replace('x', '*')[11:]).replace('x', '*'))
+        msg = message.content
         if len(calculated) < 1800:
             await client.send_message(message.channel, str(calculated))
         else:
             with open('answer.txt', 'w') as file:
                 file.write(calculated)
-            await client.send_file(message.channel, './answer.txt', filename='answer_to_'+message.content[11:].replace(' ', '_'), content=message.content[11:])
-    await client.change_presence(status=discord.Status.online, game=discord.Game(name='type !help - Last completed command: '+message.content))
+            await client.send_file(message.channel, './answer.txt', filename='answer to '+msg[11:], content=msg[11:])
+        msg = message.content
+        last_command = 'Calculating ' + msg[11:]
+    #time.sleep(3)
+    await client.change_presence(status=discord.Status.online, game=discord.Game(name='type !help - Last completed command: '+last_command))
 
 @client.event
 async def on_ready():
